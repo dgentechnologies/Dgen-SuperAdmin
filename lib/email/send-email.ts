8c5b226aecb4db2fef1221f844a0aa7ea1f4a830
Tirthankar Dasgupta <tirthankardasgupta913913@gmail.com>
2026-04-21 21:46:26 +0530

    fix4

--- lib/email/send-email.ts
+++ lib/email/send-email.ts
@@ -1,7 +1,5 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
-
 interface EmailPayload {
   to: string | string[];
   subject: string;
@@ -10,6 +8,7 @@ interface EmailPayload {
 }
 
 export async function sendEmail({ to, subject, html, from }: EmailPayload) {
+  const resend = new Resend(process.env.RESEND_API_KEY!);
   const { error } = await resend.emails.send({
     from: from ?? process.env.EMAIL_FROM!,
     to: Array.isArray(to) ? to : [to],
